ec6b031459ec1f914c05c09f38e3f35095b6f2e1
Jeremy Ahn <jeremy.ahn@dubitlimited.com>
2022-03-28 03:11:20 +0900

    Finish music2vec implementation

--- train2.py
+++ train2.py
@@ -2,5 +2,5 @@ import torch
 from models import Music2VecModel
 
 model = Music2VecModel()
-result = model(torch.arange(22050*12.8).view(1, -1))
+result = model(torch.arange(22050*12.8*2).view(2, 1, -1))
 print(result.shape)
