d3df7916b02111c948daacc19a021c3881978ec5
Jeremy Ahn <jeremy.ahn@dubitlimited.com>
2022-04-07 20:47:15 +0900

    Revert to old database

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ import julius
 import torchaudio
 import random
 import math
+import numpy as np
 from glob import glob
 from torch.utils.data import Dataset
 from torchaudio_augmentations import *
@@ -53,6 +54,66 @@ def get_audio(file_path, audio_length=12.8, target_sr=22050):
 
     return audio, attention_mask
 
+def get_audio_slices(audio_file_path, label_file_path, audio_length=12.8, target_sr=22050):
+    audio_slices = []
+    annotations = []
+
+    loaded_audio, loaded_audio_sr = torchaudio.load(audio_file_path)
+    loaded_audio_length = loaded_audio.size(dim=1) // loaded_audio_sr
+    target_audio_length = int(audio_length*target_sr)
+
+    # sampling control
+    if loaded_audio_sr != target_sr:
+        loaded_audio = julius.resample_frac(loaded_audio, loaded_audio_sr, target_sr)
+
+    if loaded_audio.size(dim=1) < target_audio_length:
+        loaded_audio, attention_mask = pad(loaded_audio)
+        audio_slices.append(loaded_audio)
+    elif loaded_audio.size(dim=1) > target_audio_length:
+        attention_mask = None
+
+        slice_count = math.ceil(loaded_audio_length / audio_length)
+        slice_remainder = loaded_audio_length % audio_length
+        slice_overlap = (audio_length - slice_remainder)/(slice_count - 1)
+
+        slice_start_times = []
+
+        for slice_index in range(slice_count):
+            slice_start = math.ceil((audio_length - slice_overlap)*slice_index*target_sr)
+            slice_length = int(audio_length*target_sr)
+            audio_slices.append(loaded_audio.narrow(1, slice_start, slice_length))
+            slice_start_times.append(slice_start)
+
+        slice_index = 0
+        slice_annotations = []
+        with open(label_file_path, 'r') as fp:
+            for _, line in enumerate(fp.readlines()):
+                current_slice_start_time = slice_start_times[slice_index]/target_sr
+                current_slice_end_time = current_slice_start_time + audio_length
+
+                time, beat_number = line.strip('\n').split(' ')
+                time = round(float(time), 4)
+                beat_number = int(beat_number)
+
+                is_downbeat = 1 if beat_number == 1 else 0
+
+                while time > current_slice_end_time and slice_index + 1 < slice_count:
+                    slice_index += 1
+                    current_slice_start_time = slice_start_times[slice_index]/target_sr
+                    current_slice_end_time = current_slice_start_time + audio_length
+
+                    if len(slice_annotations) > 0:
+                        print("new slice")
+                        annotations.append(slice_annotations)
+                        slice_annotations.clear()
+
+                if time <= audio_length:
+                    print("new annotation")
+                    relative_time = time - current_slice_start_time
+                    slice_annotations.append([relative_time, is_downbeat])
+
+    return audio_slices, annotations
+
 class BeatDataset():
     def __init__(self, path, audio_length=12.8, sr=22050):
         '''
@@ -69,42 +130,56 @@ class BeatDataset():
         self.audio_length = audio_length
         self.sr = sr
 
+        self.audio_slices = []
+        self.annotations = []
+        print("Starting data processing")
         with open(os.path.join(path, 'new_data.txt'), 'r') as fp:
             for line in fp.readlines():
-                #self.data.append(line.strip('\n'))
-                file_path = line.strip('\n')
-                loaded_audio, loaded_audio_sr = torchaudio.load(file_path)
-                loaded_audio_length = loaded_audio.size(dim=1) // loaded_audio_sr
+                audio_file_path = line.strip('\n')
+                if audio_file_path.find(".wav"):
+                    label_file_path = audio_file_path.replace(".wav", ".beats")
+                elif audio_file_path.find(".mp3"):
+                    label_file_path = audio_file_path.replace(".mp3", ".beats")
 
-                audio_slice_count = math.ceil(loaded_audio_length / audio_length)
-                audio_slice_remainder = loaded_audio_length % audio_length
-                audio_slice_overlap = audio_slice_remainder / audio_slice_count
+                if label_file_path:
+                    label_file_path = label_file_path.replace("/data/", "/label/")
+                    new_audio_slices, new_annotations = get_audio_slices(
+                        audio_file_path,
+                        label_file_path,
+                        audio_length,
+                        sr
+                    )
 
-                for slice_index in range(audio_slice_count):
-                    self.data.append()
+                    print("slice", len(new_audio_slices))
+                    print("annotation", len(new_annotations))
+
+                    self.audio_slices += new_audio_slices
+                    self.annotations += new_annotations
+
+        print("Finishing data processing")
 
     def __len__(self):
-        #return len(self.label)
-        return self.audio_slice_count
+        return len(self.data)
 
     def __getitem__(self, idx):
-        audio = get_audio(self.data[idx], self.audio_length, self.sr)
+        # audio = get_audio(self.data[idx], self.audio_length, self.sr)
 
-        annotations = []
+        # annotations = []
 
-        filename = self.label[idx]
+        # filename = self.label[idx]
 
-        with open(filename, 'r') as fp:
-            for _, line in enumerate(fp.readlines()):
-                time_start, time_end, is_downbeat = line.strip('\n').split('\t')
-                time_start = round(float(time_start), 4)
-                time_end = round(float(time_end), 4)
-                is_downbeat = int(is_downbeat)
+        # with open(filename, 'r') as fp:
+        #     for _, line in enumerate(fp.readlines()):
+        #         time_start, time_end, is_downbeat = line.strip('\n').split('\t')
+        #         time_start = round(float(time_start), 4)
+        #         time_end = round(float(time_end), 4)
+        #         is_downbeat = int(is_downbeat)
 
-                if time_start < self.audio_length or time_end <= self.audio_length:
-                    annotations.append([time_start, time_end, is_downbeat])
+        #         if time_start < self.audio_length or time_end <= self.audio_length:
+        #             annotations.append([time_start, time_end, is_downbeat])
 
-        return audio, annotations
+        # return audio, annotations
+        return self.audio_slices[idx], self.annotations[idx]
         
     def random_crop(self,item):
         crop_size = int(self.sequence_len * self.sr)
