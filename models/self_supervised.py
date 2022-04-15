7c13cbe5128ee1f3f8bc0b8cb3e9a7a09c8385ec
Jeremy Ahn <jeremy.ahn@dubitlimited.com>
2022-04-16 03:33:00 +0900

    set device

--- models/self_supervised.py
+++ models/self_supervised.py
@@ -307,6 +307,9 @@ class Music2VecModel(nn.Module):
             batch_size, sequence_length, _ = extract_x.size()
             attention_mask = torch.zeros((batch_size, sequence_length), dtype=torch.int32)
 
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            attention_mask.to(device)
+
             mask_criterion = torch.lt(lengths, sequence_length)
             mask_criterion_sum = mask_criterion.sum()
             if mask_criterion_sum != 0:
